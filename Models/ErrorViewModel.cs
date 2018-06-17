fb11631fc929941ea69e15cf1849901afaf0a748
Nathaniel Glover <nathanielsglover@gmail.com>
2018-06-17 16:35:35 -0400

    Create default ASP.NET Core site

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WahsKeyClubSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WahsKeyClubSite
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<string>();
+            list.AddRange(args);
+            list.Add(" --server.urls");
+            BuildWebHost(list.ToArray()).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
